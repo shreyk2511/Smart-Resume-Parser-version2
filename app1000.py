9b5a79d521561dc385f2ed09eaad71d0a46e0bed
Shreya Shettigar <83949234+shreyk2511@users.noreply.github.com>
2023-03-10 21:58:00 +0530

    Update app1000.py

--- app1000.py
+++ app1000.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from resume_parser import ResumeParser
 
-pdf = "C:\\Users\\shrey\\pythonProject\\CV (Manisha jain) doc-converted.pdf"
+pdf = "CV (Manisha jain) doc-converted.pdf"
 data = ResumeParser(pdf).get_extracted_data()
 st.write(data)
