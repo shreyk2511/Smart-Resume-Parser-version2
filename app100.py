138ae567ac013ce896bc13bef989d815749b92cf
Shreya Shettigar <83949234+shreyk2511@users.noreply.github.com>
2023-03-10 12:22:55 +0530

    Update app100.py

--- app100.py
+++ app100.py
@@ -1,5 +1,8 @@
 import resume_parser
 import streamlit as st
+import nltk
+import spacy
+nlp = spacy.load("en_core_web_sm")
 myresume="CV (Manisha jain) doc-converted.pdf"
 data = resume_parser.ResumeParser(myresume).get_extracted_data()
 st.write(data)
