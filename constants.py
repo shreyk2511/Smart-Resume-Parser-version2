a2e95051843f7ddcebc21543677e001792186540
Shreya Shettigar <83949234+shreyk2511@users.noreply.github.com>
2023-03-10 11:22:35 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-from nltk.corpus import stopwords
+
 
 # Omkar Pathak
 NAME_PATTERN = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
@@ -22,7 +22,17 @@ MONTHS_LONG = r'''(january)|(february)|(march)|(april)|(may)|(june)|(july)|
 MONTH = r'(' + MONTHS_SHORT + r'|' + MONTHS_LONG + r')'
 YEAR = r'(((20|19)(\d{2})))'
 
-STOPWORDS = set(stopwords.words('english'))
+df = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves",
+      "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
+      "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are",
+      "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an",
+      "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about",
+      "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up",
+      "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when",
+      "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor",
+      "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should",
+      "now"]
+STOPWORDS = set(df)
 
 RESUME_SECTIONS_PROFESSIONAL = [
                     'experience',
